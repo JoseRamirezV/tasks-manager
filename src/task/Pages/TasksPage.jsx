15e0721b9b551759053da04e4c70d3cd6db1e2bc
Harold Moreno <haroldmoreno720@gmail.com>
2023-09-05 18:00:22 -0500

    ADD - Creado component TaskCard @author:HaroldsCode

--- src/task/Pages/TasksPage.jsx
+++ src/task/Pages/TasksPage.jsx
@@ -1,15 +1,37 @@
-import { Box } from '@chakra-ui/react'
-import { Helmet } from 'react-helmet-async'
+import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Helmet } from "react-helmet-async";
+import { useEffect, useState } from "react";
+
+import useTasks from "../hooks/useTasks";
+import { TaskCard } from "../components/TaskCard";
 
 const TasksPage = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useTasks()
+      .then(res => setData(res))
+      .catch(() => setData([]))
+
+    return () => {
+      setData([]);
+    };
+  }, []);
+
   return (
     <>
       <Helmet>
         <title>Mis tareas | Taskty</title>
       </Helmet>
-      <Box as="section">Tareas</Box>
+      <Box as="section" py={4}>
+        <Heading as={'h1'} width={'100%'} pb={6} textAlign={'center'}> Tareas </Heading>
+        <SimpleGrid columns={[1, null, 2]} spacing="2rem">
+          {data.length > 0 && data.map((data) => (<TaskCard key={data.id} task={data}/>))}
+        </SimpleGrid>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default TasksPage
+export default TasksPage;
